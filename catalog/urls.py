b7f1e0a42796a7e735d241ea4eee20d8cf31e59a
razmyslovartem <razmyslovartem@icloud.com>
2026-06-05 15:51:30 +0300

    update urls.py and views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
+
+
+# Задаем пространство имен в файле маршрутизации приложения.
+app_name = "catalog"
 
 urlpatterns = [
-    path("", ),
+    path("", views.home, name="home"),  # 127.0.0.1:8000/
+    path("contacts/", views.contacts, name="contacts"),  # 127.0.0.1:8000/contacts/
+    path("contact/", views.contacts, name="contact"),  # 127.0.0.1:8000/contact/
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def contacts(request):
+    return render(request, "catalog/contacts.html")
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        return HttpResponse(f"Спасибо, {name} ваш номер:{phone} и сообщение:{message} отправлены!")
+    return render(request, "catalog/contacts.html")

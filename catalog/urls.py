c21ed58bef99d4de8bc2744af1dfdea1741fc224
razmyslovartem <razmyslovartem@icloud.com>
2026-06-05 15:54:13 +0300

    update catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,7 @@ app_name = "catalog"
 
 urlpatterns = [
     path("", views.home, name="home"),  # 127.0.0.1:8000/
+    path('home/', views.home, name='home_alt'),  # 127.0.0.1:8000/home/
     path("contacts/", views.contacts, name="contacts"),  # 127.0.0.1:8000/contacts/
     path("contact/", views.contacts, name="contact"),  # 127.0.0.1:8000/contact/
 ]

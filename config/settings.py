2ace59a9e83e500fea84cafdebcc9b9acb25d456
razmyslovartem <razmyslovartem@icloud.com>
2026-06-05 15:20:50 +0300

    create startApp catalog

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dx*zl&%g!i4mt4dcs51m65aqf!y@%%ifu$t9b73ev#l0gv)zhi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

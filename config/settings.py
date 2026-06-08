498b1d50922fde3f344d714ecec28dd781cb5392
razmyslovartem <razmyslovartem@icloud.com>
2026-06-08 13:51:22 +0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # тип СУБД
+        'NAME': 'mydatabase',  # имя базы данных
+        'USER': 'mydatabaseuser',  # имя пользователя PostgreSQL
+        'PASSWORD': 'mypassword',  # пароль пользователя PostgreSQL
+        'HOST': 'localhost',  # адрес сервера базы данных
+        'PORT': '5432',  # порт, на котором работает PostgreSQL, обычно 5432
     }
 }
 

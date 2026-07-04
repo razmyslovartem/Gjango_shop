4c437122a3885d552f64f26e573a404387824de9
Artem Razmyslov <89083284426@mail.ru>
2026-07-04 15:32:47 +0300

    update catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,6 @@ app_name = "catalog"
 urlpatterns = [
     path("", views.catalog_list, name="catalog_list"),
     path("product/<int:pk>/", views.catalog_detail, name="catalog_detail"),
-    path("contacts/", views.contacts, name="contacts"),
+    path("contacts/", views.contacts, name="contacts"),  # GET - показать форму
+    path("contact/", views.contact, name="contact"),  # POST - обработать форму
 ]

f0abcdf5a400c052ceb8f27311b8c35d20879fb0
Artem Razmyslov <89083284426@mail.ru>
2026-07-18 11:27:50 +0300

    update html files

--- catalog/views.py
+++ catalog/views.py
@@ -1,35 +1,66 @@
 # catalog/views.py
 
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, FormView, ListView, UpdateView
+
 from blog.models import BlogPost
+from catalog.forms import ContactForm, ProductForm
 from catalog.models import Product
-from django.views.generic import DetailView
-from django.views.generic import ListView
-from django.views.generic import TemplateView
 
 
+# Create - создание продукта.
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        """Редирект на страницу созданного продукта."""
+        return reverse_lazy("catalog:product_detail", kwargs={"pk": self.object.pk})
+
+
+# Read - весь список.
 class ProductListView(ListView):
     model = Product
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Добавляем последнюю опубликованную статью
         context["latest_post"] = BlogPost.objects.filter(is_published=True).order_by("-created_at").first()
         return context
 
 
+# Read - детально продукт.
 class ProductDetailView(DetailView):
     model = Product
 
 
-class ContactTemplateView(TemplateView):
+# Update - редактирование продукта.
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        """Редирект на страницу отредактированного продукта."""
+        return reverse_lazy("catalog:product_detail", kwargs={"pk": self.object.pk})
+
+
+# Delete - удаление продукта.
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ContactFormView(FormView):
     template_name = "catalog/contacts.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("catalog:contacts")
 
-    def post(self, request, *args, **kwargs):
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
+    def form_valid(self, form):
+        # Получаем данные из формы
+        name = form.cleaned_data["name"]
+        phone = form.cleaned_data["phone"]
+        message = form.cleaned_data["message"]
 
+        # Здесь можно отправить email, сохранить в БД и т.д.
         print(f"Имя: {name}, Телефон: {phone}, Сообщение: {message}")
 
-        context = self.get_context_data(success=True, name=name, phone=phone, message=message)
-        return self.render_to_response(context)
+        return super().form_valid(form)
